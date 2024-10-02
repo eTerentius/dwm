1ae2a3a769a0278472cca959f4ebf44e4e740ad1
eVarro <82235375+eTerentius@users.noreply.github.com>
2024-10-02 14:58:41 -0500

    Update debian-core-install.sh

--- debian-core-install.sh
+++ debian-core-install.sh
@@ -71,8 +71,6 @@ install_packages() {
 # Call function to install packages
 install_packages "${packages[@]}"
 
-
-
 # Ensure /usr/share/xsessions directory exists
 if [ ! -d /usr/share/xsessions ]; then
     sudo mkdir -p /usr/share/xsessions
@@ -82,6 +80,47 @@ if [ ! -d /usr/share/xsessions ]; then
     fi
 fi
 
-sudo apt autoremove
+clear
+printf "\n\e[1;32mInstalling dwm.\e[0m\n"
 
-source ./dwm-suckless-tools.sh
+## Get dwm and suckless-tools source from debian
+mkdir -p $HOME/.config/suckless
+(cd $HOME/.config/suckless; apt source dwm)
+(cd $HOME/.config/suckless; rm -rf *.tar.gz *.tar.xz *.dsc)
+
+# Install dwm
+(
+cd $HOME/.config/suckless/dwm*;
+        if make; then
+            make_success=true
+        fi
+        if $make_success; then
+            break
+        fi
+)
+(
+cd $HOME/.config/suckless/dwm*;
+        if sudo make install clean; then
+            install_success=true
+        fi
+        if $install_success; then
+            break
+        fi
+)
+
+# Enable and disable services
+sudo systemctl stop avahi-daemon.socket
+sudo systemctl disable avahi-daemon.socket
+sudo systemctl mask avahi-daemon.socket
+sudo systemctl stop avahi-daemon.service
+sudo systemctl disable avahi-daemon.service
+sudo systemctl mask avahi-daemon.service
+sudo systemctl enable acpid
+
+# Cleanup
+sudo apt autoremove && sudo apt autoclean
+
+#-- Kill sudo --#
+stopsudo
+
+printf "\n\e[1;32mYou can now reboot!\e[0m\n"
